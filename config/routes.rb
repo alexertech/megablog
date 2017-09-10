a877d2fa0f60024490c7120508e44586071df4b6
Alex B <ab@aexl.local>
2017-09-09 21:50:16 -0300

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'posts#index'
+  resources :posts
+
+  # Resources avoid to write all this
+  # get '/posts',             to: 'posts#index'
+  # get '/posts/new',         to: 'posts#new'
+  # get '/posts/:id',         to: 'posts#show'
+  # get '/posts/:id/edit',    to: 'posts#edit'
+  # post '/posts',            to: 'posts#create'
+  # patch '/posts/:id',       to: 'posts#update'
+  # delete '/posts/:id',      to: 'posts#destroy'
+
+
+
 end
